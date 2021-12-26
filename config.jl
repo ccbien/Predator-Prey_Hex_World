7580da5e7542e0da49907842325808832494b882
ccbien <chibien408@gmail.com>
2021-12-26 22:16:14 +0700

    update config

--- config.jl
+++ config.jl
@@ -1,25 +1,22 @@
-using Parameters
+using Parameters: @with_kw
 
 @with_kw struct Config
     n_rows::Int32
     n_cols::Int32
     num_predators::Int32
     num_preys::Int32
-    ε::Float32 # ε-greedy
-    
-    epochs::Int32
-    steps::Int32
+    num_iterations::Int32
+    num_steps::Int32
 end
 
 function get_config_1()::Config
     kwargs = Dict(
-        :n_rows => 5,
-        :n_cols => 5,
+        :n_rows => 4,
+        :n_cols => 8,
         :num_predators => 2,
         :num_preys => 2,
-        :ε => 0.1,
-        :epochs => 10,
-        :steps => 1000
+        :num_iterations => 100,
+        :num_steps => 100
     )
     return Config(;kwargs...)
 end
